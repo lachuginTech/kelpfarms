ea0bacd7e97e9b1388b25ebcc9813f5728203378
lachuginTech <vladimir.lachugin.tech@gmail.com>
2024-10-26 22:04:02 +0300

    maps

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // бургер
     const btnBurger = document.querySelector('.hamburger');
     const nav = document.querySelector('.header__list');
 
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    // select
     const selectBox = document.querySelector('.select-box');
     const optionsContainer = document.querySelector('.options');
     const options = document.querySelectorAll('.option');
@@ -21,12 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
         option.addEventListener('click', (e) => {
             const selectedValue = e.target.textContent;
             selectBox.textContent = selectedValue;
-            hiddenInput.value = selectedValue; // Передаем выбранное значение в скрытое поле
+            hiddenInput.value = selectedValue;
             selectBox.classList.remove('active');
         });
     });
 
-    // Закрытие списка при клике вне его
+
     document.addEventListener('click', (e) => {
         if (!selectBox.contains(e.target)) {
             selectBox.classList.remove('active');
@@ -39,6 +41,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function getOptionValue(e) {
     console.log(e.target.checked);
+    }
+
+    // text
+});
+
+function truncateText(element, maxHeight) {
+    let text = element.innerText;
+    let words = text.split(' ');
+
+    
+    element.innerText = text;
+
+   
+    if (element.scrollHeight <= maxHeight) return;
+
+
+    while (element.scrollHeight > maxHeight && words.length > 0) {
+        words.pop();
+        element.innerText = words.join(' ') + '...';
+    }
 }
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.news__text').forEach(element => {
+        const parent = element.closest('.news__inner');
+        const title = parent.querySelector('.news__title');
+        const link = parent.querySelector('.news__link');
+        
+        const titleHeight = title ? title.offsetHeight : 0;
+        const linkHeight = link ? link.offsetHeight : 0;
+
+        const maxHeight = 320 - titleHeight - linkHeight - 65;
+
+        truncateText(element, maxHeight);
+    });
 });
